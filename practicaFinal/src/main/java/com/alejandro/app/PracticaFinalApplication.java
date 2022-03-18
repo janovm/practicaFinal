f10cb6658d7091b0d53b12d7d62d0bd696ff2164
Alejandro Casado <jano3708@hotmail.com>
2022-03-18 08:30:54 +0100

    empiezo proyecto

---
+++ practicaFinal/src/main/java/com/alejandro/app/PracticaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFinalApplication.class, args);
+	}
+
+}
